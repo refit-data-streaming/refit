b36413b7e68a3ee5f5212b65e4aec08b2d1da561
Raman Khurana <ramankhurana1986@gmail.com>
2024-01-19 10:04:17 -0600

    Update KafkaSensorDataRepository.scala
    edited log again, want to log the topic name.

--- data/data-kafka/src/main/scala/edu/cdl/iot/data/kafka/repository/KafkaSensorDataRepository.scala
+++ data/data-kafka/src/main/scala/edu/cdl/iot/data/kafka/repository/KafkaSensorDataRepository.scala
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory
 class KafkaSensorDataRepository(kafkaRepository: KafkaRepository) {
   private val logger = LoggerFactory.getLogger(classOf[KafkaSensorDataRepository])
   def createSensorData(sensorData: SensorData): Unit = {
-    logger.info("sensorData ready to send to kafka topics 'data'  {}",sensorData)
+    logger.info("sensorData ready to send to kafka topics 'data'  {}",kafkaRepository.topics.data)
     kafkaRepository.send(kafkaRepository.topics.data, sensorData.toByteArray)
   }
 }
